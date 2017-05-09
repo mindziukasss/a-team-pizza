e0166a350c8833751f8d554c970ec5dcd000ac74
Agne <agnepetrylaite@gmail.com>
2017-05-09 10:42:46 +0300

    migrations padaryta

---
+++ database/migrations/2017_05_09_073220_create_tm_cheeses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateTmCheesesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tm_cheeses', function(Blueprint $table)
+		{
+			$table->integer('count', true);
+			$table->string('id', 36)->unique('id_UNIQUE');
+			$table->timestamps();
+			$table->softDeletes();
+			$table->string('name');
+			$table->string('calories', 45)->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tm_cheeses');
+	}
+
+}
