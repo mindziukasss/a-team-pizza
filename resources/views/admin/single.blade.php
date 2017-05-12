29c322155c5b3a8bf6722811594fa2c4ec3f6f57
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-05-12 14:51:51 +0300

    Created all form parse

--- resources/views/admin/single.blade.php
+++ resources/views/admin/single.blade.php
@@ -1,5 +1,54 @@
 @extends('admin.admin')
 
 @section('content-list')
-    {{var_dump($pizzas)}}
+    <div class="container">
+        <table class="table table-bordered">
+            <thead>
+            @foreach($single as $key => $value)
+                <tr>
+                    <td>{{$key}}</td>
+                    <td>{{$value}}</td>
+                </tr>
+            @endforeach
+
+            </thead>
+
+        </table>
+        <button onclick="deleteItem('{{ route($deleteRoute, $single['id']) }}')" class="btn btn-danger">
+            Delete
+        </button>
+        <a href="{{ route($editRoute, $single['id']) }}">
+            <button type="button" class="btn btn-primary">Edit</button>
+        </a>
+        <a href="{{ route($list) }}">
+            <button type="button" class="btn btn-default">Back</button>
+        </a>
+    </div>
+
+@endsection
+
+@section('scripts')
+    <script>
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        function deleteItem(route) {
+            $.ajax({
+                url: route,
+                type: 'DELETE',
+                //data : {},
+                dataType: 'json',
+                success: function () {
+                    alert('DELETED');
+                },
+                error: function () {
+                    alert('ERROR');
+                }
+            });
+        }
+    </script>
 @endsection
