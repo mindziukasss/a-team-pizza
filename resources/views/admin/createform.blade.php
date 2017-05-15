9dce3596190311711a15f8fdc9160824e5d878b9
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-05-15 15:26:38 +0300

    Fixed token pz_users

--- resources/views/admin/createform.blade.php
+++ resources/views/admin/createform.blade.php
@@ -1,34 +1,19 @@
 @extends('admin.admin')
+
 @section('content-list')
+    <div class ="row">
+        <div class="col-md-10">
+            <div class="form">
+                {!! Form::open(['url' => route($createRoute)]) !!}
 
-    {{--@if(isset($error))--}}
-    {{--<h4 style="color:red">{{ $error['message'] }}</h4>--}}
-    {{--@endif--}}
-    {{--@if(isset($success))--}}
-    {{--<h4 style="color:green">{{ $success['message'] }}</h4>--}}
-    {{--@endif--}}
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                {!! Form::model(['url' => route($route)]) !!}
-                <table class="table table-bordered">
-                    @foreach($item as $key => $value)
-                        @if($value != 'created_at' && $value != 'updated_at' && $value != 'deleted_at' && $value != 'id' && $value != 'count')
-                            <tr>
-                                <td>{{$value}}</td>
-                                <td>
-                                    {{ Form::label('', null, ['class' => 'control-label']) }}
-                                    {{ Form::text($value) }}
-                                </td>
-                            </tr>
-                        @endif
-                    @endforeach
-                </table>
-                {{Form::submit('Submit!')}}
-
-                @if(isset($data))
-                    <p>testas</p>
-                @endif
+                <h1>Create</h1>
+                {{ Form::label('name', 'Name') }}
+                {{ Form::text('name') }}
+                {{ Form::label('calories', 'Calories' ) }}
+                {{ Form::text('calories') }}
+                {{ Form::submit('Create', ['class'=>'btn btn-primary']) }}
+                {!! Form::close() !!}
             </div>
         </div>
     </div>
+@endsection
--- resources/views/admin/editform.blade.php
+++ resources/views/admin/editform.blade.php
@@ -1,26 +1,19 @@
 @extends('admin.admin')
-@section('content-list')
 
-    <div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            {!! Form::open(['url' => route($route, $id)]) !!}
-            <table class="table table-bordered">
-            @foreach($item as $key => $value)
-                @if($key != 'created_at' && $key != 'updated_at' && $key != 'deleted_at' && $key != 'id' && $key != 'count')
-                <tr>
-                    <td>{{$key}}</td>
-                    <td>
-                        {{ Form::label('', null, ['class' => 'control-label']) }}
-                        {{ Form::text($key, $value) }}
-                    </td>
-                </tr>
-                @endif
-            @endforeach
-            </table>
-            {{Form::submit('Submit!')}}
+@section('content-list')
+    <div class ="row">
+        <div class="col-md-10">
+            <div class="form">
+                {!! Form::open(['url' => $editRoute]) !!}
+
+                <h1>Edit</h1>
+                {{ Form::label('name', 'Name') }}
+                {{ Form::text('name', $singleEdit['name'] ) }}
+                {{ Form::label('calories', 'Calories' ) }}
+                {{ Form::text('calories', $singleEdit['calories']) }}
+                {{ Form::submit('Create', ['class'=>'btn btn-primary']) }}
+                {!! Form::close() !!}
+            </div>
         </div>
     </div>
-    </div>
-
 @endsection
