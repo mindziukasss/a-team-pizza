6c8678b1ca977fa4fc345acefee8a11b409da34d
Agne <agnepetrylaite@gmail.com>
2017-05-18 15:55:58 +0300

    cching special ingredient

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\commands\CreateAdministrator;
+use App\Console\Commands\SpecialIngredient;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         CreateAdministrator::class,
+        SpecialIngredient::class,
     ];
 
     /**
@@ -25,8 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('pick:ingredient')->everyMinute();
     }
 
     /**
